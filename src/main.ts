880b8709b958856d9342d2dee3fe0a4f091c800b
Kenson Johnson <kenson.johnson@outlook.com>
2022-12-26 15:23:30 -0600

    Built out the UI

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const searchInput = document.querySelector(
+  "[data-searchbar]"
+) as HTMLInputElement;
+
+const searchClearButton = document.querySelector(
+  "[data-clear-button]"
+) as HTMLButtonElement;
+
+searchClearButton.addEventListener("click", () => {
+  searchInput.value = "";
+  console.log("clicked");
+});
